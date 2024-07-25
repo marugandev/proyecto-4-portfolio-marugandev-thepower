568b7126d7085734b09c51bdabda57d3f84b44c3
maruganjavier <javiermarugan4ocio@gmail.com>
2024-07-26 00:25:03 +0200

    FIX RENDER IMAGES CONTACT

--- src/data/ContactData/ContactData.js
+++ src/data/ContactData/ContactData.js
@@ -4,28 +4,28 @@ export const ContactData = [
       href: "mailto:marugandev@gmail.com",
       target: "_blank",
       title: "Email",
-      imgSrc: "/public/assets/icons/email-icon-positivo.svg",
+      imgSrc: "/assets/icons/email-icon-positivo.svg",
       imgAlt: "Email icono"
     },
     linkedin: {
       href: "https://www.linkedin.com/in/javiermarugangarcia/",
       target: "_blank",
       title: "Linkedin",
-      imgSrc: "/public/assets/icons/linkedin-logo-positivo.svg",
+      imgSrc: "/assets/icons/linkedin-logo-positivo.svg",
       imgAlt: "Linkedin logo"
     },
     github: {
       href: "https://github.com/marugandev",
       target: "_blank",
       title: "Github",
-      imgSrc: "/public/assets/icons/github-logo-positivo.svg",
+      imgSrc: "/assets/icons/github-logo-positivo.svg",
       imgAlt: "Github logo"
     },
     figma: {
       href: "#",
       target: "_blank",
       title: "Figma",
-      imgSrc: "/public/assets/icons/figma-logo-positivo.svg",
+      imgSrc: "/assets/icons/figma-logo-positivo.svg",
       imgAlt: "Figma logo"
     }
   }
